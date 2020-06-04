783b5b0509aed37a4b4507445c9f158eff7dc522
Karahan <50397005+bootloaderx@users.noreply.github.com>
2020-06-04 23:44:09 +0300

    Update and rename test.java to SetMetodu.java

--- src/main/java/SetMetodu.java
+++ src/main/java/SetMetodu.java
@@ -1,22 +1,22 @@
+//Set() Metodu
 import java.util.*;
 
-//Set() Metodu kod örneği
-public class test {
+public class SetMetodu {
    public static void main(String[] args) {
       
     ArrayList<String> renkler = new ArrayList<String>(5);
 
-    // Listeye eleman eklemek için add metotunu kullandım
+    // Listeye eleman eklemek için add metotunu kullandım.
     renkler.add("Beyaz");
     renkler.add("Siyah");
     renkler.add("Kırmızı");
     renkler.add("Yeşil");
-	  renkler.add("Sarı");
+    renkler.add("Sarı");
 	    
-	// Kırmızı rengi Mürdüm rengiyle değiştireceğim 
+    // Kırmızı rengi Mürdüm rengiyle değiştireceğim.
     renkler.set(2, new String("Mürdüm"));		
 	
-	// Şimdi de renkleri yazdırıyorum
+    // Şimdi de renkleri yazdırıyorum
     for (int i = 0; i < 5; i++)
       {
          System.out.println(renkler.get(i).toString());
